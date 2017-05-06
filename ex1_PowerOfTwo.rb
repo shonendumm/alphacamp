243fa291c9c0f8a3c8c1786ef381ade63462dff7
Soo Hian Linux <shonendumm@gmail.com>
2017-05-06 21:36:55 +0800

    Change name of method

--- ex1_PowerOfTwo.rb
+++ ex1_PowerOfTwo.rb
@@ -1,7 +1,7 @@
 # Exercise 1: Power of Two
 # Given an integer, write a function to determine if it is a power # of two. 
 
-def power(n)
+def is_power_of_two(n)
 	if n % 2 != 0 #to check if it's divisible by two
 		puts "#{n} is not a power of two." 
 		return false
@@ -11,7 +11,7 @@ def power(n)
 		x +=1 #increase the counter each time
 		case
 		when 2**x == n
-			puts "#{n} is 2 power of #{x}"
+			puts "#{n} is 2 to power of #{x}"
 			return true
 		when 2**x > n
 			puts "#{n} is not a power of two."
@@ -22,10 +22,10 @@ end
 end
 
 
-power(8)
-power(6363)
-power(1122334)
-power(1024)
+is_power_of_two(8)
+is_power_of_two(6363)
+is_power_of_two(1122334)
+is_power_of_two(1024)
 
 
 
