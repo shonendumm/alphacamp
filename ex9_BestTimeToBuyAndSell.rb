cc131307732b8cbd3a3f06569846fd98c2b332ef
Soo Hian Linux <shonendumm@gmail.com>
2017-05-08 00:20:24 +0800

    Initialise exercise

---
+++ ex9_BestTimeToBuyAndSell.rb
@@ -0,0 +1,25 @@
+=begin Exercise 9: Best Time to Buy and Sell Stock II
+Say you have an array for which the ith element is the price of a given stock on day i. 
+Design an algorithm to find the maximum profit. You may complete as many transactions as you like (ie, buy one and sell one share of the stock multiple times). However, you may not engage in multiple transactions at the same time
+(ie, you must sell the stock before you buy again).
+# @param {Integer[]} prices
+# @return {Integer}
+
+=end
+
+
+def max_profit(prices)
+
+def when_to_buy
+# Buy during the early bullish phase. Calculate what's the 30-day moving average of the stock's price as comparison. Buy when the price over 5-10 days show a pattern of average higher price than the moving average. http://www.businessinsider.com/the-best-time-to-buy-and-sell-stocks-2016-4?IR=T&r=US&IR=T
+
+
+
+
+
+def when_to_sell
+# Sell during the distribution phase, before the decline phase. This is when the shorter term moving averages cross each other, indicating a confusion of trend. 
+
+
+
+end
